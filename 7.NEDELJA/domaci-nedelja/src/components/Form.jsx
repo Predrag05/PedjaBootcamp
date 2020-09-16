e17e299a032bdc142f3979f6c20bb92cb1e8f594
Predrag05 <sim.pedja@gmail.com>
2020-09-17 00:49:51 +0200

    domaci

---
+++ 7.NEDELJA/domaci-nedelja/src/components/Form.jsx
@@ -0,0 +1,11 @@
+import React from 'react'; 
+
+const Form = ({string}) => {
+    return (
+        <>
+            <input type='text'/>
+            <button>{string}</button>
+        </>
+    )
+};
+export default Form;
